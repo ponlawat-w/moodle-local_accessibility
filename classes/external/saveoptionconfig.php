d4de51cd0e42589d5a0323f0218f752eb7c01fe7
Ponlawat W <ponlawat_w@cmu.ac.th>
2023-05-16 19:46:34 +0700

    Updated external class namespaces

--- classes/external/saveoptionconfig.php
+++ classes/external/saveoptionconfig.php
@@ -2,26 +2,22 @@
 
 namespace local_accessibility\external;
 
-use external_function_parameters;
-use external_single_structure;
-use external_value;
-use moodle_exception;
-
 defined('MOODLE_INTERNAL') or die();
 
 require_once(__DIR__ . '/../../lib.php');
+require_once(__DIR__ . '/../../../../lib/externallib.php');
 
-class saveoptionconfig extends \external_api {
-    public static function execute_parameters(): external_function_parameters {
-        return new external_function_parameters([
-            'optionname' => new external_value(PARAM_ALPHANUMEXT, 'Option Name', VALUE_REQUIRED, '', NULL_NOT_ALLOWED),
-            'configvalue' => new external_value(PARAM_TEXT, 'Configuration Value', VALUE_OPTIONAL, null, NULL_ALLOWED)
+class saveoptionconfig extends \core_external\external_api {
+    public static function execute_parameters(): \core_external\external_function_parameters {
+        return new \core_external\external_function_parameters([
+            'optionname' => new \core_external\external_value(PARAM_ALPHANUMEXT, 'Option Name', VALUE_REQUIRED, '', NULL_NOT_ALLOWED),
+            'configvalue' => new \core_external\external_value(PARAM_TEXT, 'Configuration Value', VALUE_OPTIONAL, null, NULL_ALLOWED)
         ]);
     }
 
-    public static function execute_returns(): external_single_structure {
-        return new external_single_structure([
-            'success' => new external_value(PARAM_BOOL, 'True if success')
+    public static function execute_returns(): \core_external\external_single_structure {
+        return new \core_external\external_single_structure([
+            'success' => new \core_external\external_value(PARAM_BOOL, 'True if success')
         ]);
     }
 
