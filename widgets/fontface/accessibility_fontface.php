3e0a51464dae5373826c75efe00682293520c579
Ponlawat W <ponlawat_w@cmu.ac.th>
2023-07-14 12:52:40 +0700

    Added widget comments and documentation

--- widgets/fontface/accessibility_fontface.php
+++ widgets/fontface/accessibility_fontface.php
@@ -1,9 +1,34 @@
 <?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Widget definition
+ *
+ * @package     accessibility_fontface
+ * @copyright   2023 Ponlawat Weerapanpisit <ponlawat_w@outlook.co.th>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 namespace local_accessibility\widgets;
 
 defined('MOODLE_INTERNAL') or die();
 
+/**
+ * Font face accessibility widget definition
+ */
 class fontface extends widgetbase {
     public function __construct() {
         parent::__construct(get_string('pluginname', 'accessibility_fontface'), 'fontface');
