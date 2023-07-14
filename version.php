37090154a7b1da9de436bbf104cdcece0ae819a6
Ponlawat W <ponlawat_w@cmu.ac.th>
2023-07-14 13:27:15 +0700

    Updated version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_accessibility';
-$plugin->release = '0.0.0';
-$plugin->version = 2023071300;
+$plugin->release = '0.1.0';
+$plugin->version = 2023071400;
 $plugin->requires = 2022041900;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
